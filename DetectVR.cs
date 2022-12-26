0bb13b53d41d331ad2fd036785b0d9137036de52
vitorbcc2021 <102258365+vitorbcc2021@users.noreply.github.com>
2022-12-26 19:06:32 -0300

    Initial commit

---
+++ DetectVR.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.XR.Management;
+
+public class DetectVR : MonoBehaviour
+{
+    public bool startInVR = true;
+    public GameObject xrOrigin;
+    public GameObject desktopCharacter;
+
+
+    void Start()
+    {
+        if(startInVR)
+        {
+            var xrSettings = XRGeneralSettings.Instance;
+        
+            if(xrSettings == null)
+            {
+                Debug.Log("XRGeneralSettingss is null");
+                return;
+            }
+
+            var xrManager = xrSettings.Manager;
+
+            if(xrManager == null)
+            {
+                Debug.Log("XRManagerSettings is null");
+                return;
+            }
+
+            var xrLoader = xrManager.activeLoader;
+
+            if(xrLoader == null)
+            {
+                Debug.Log("XRLoader is null");
+                xrOrigin.SetActive(false);
+                desktopCharacter.SetActive(true);
+
+                return;
+            }
+
+            Debug.Log("XRLoader is NOT null");
+            xrOrigin.SetActive(true);
+            desktopCharacter.SetActive(false);
+        }
+        else
+        {
+            xrOrigin.SetActive(false);
+            desktopCharacter.SetActive(true);
+        }
+    }
+}
